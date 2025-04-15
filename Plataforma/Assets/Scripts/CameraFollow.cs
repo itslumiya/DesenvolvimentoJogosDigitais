7c4772e933fd7dbc76aef5fb7425375abf9a26c2
Livia Lumi Miyabara <livia3204@gmail.com>
2025-04-14 22:59:25 -0300

    [FEAT]: Adicionando Menu + Diálogos com as regras na fase 1 + Alteração da fase 2

--- Plataforma/Assets/Scripts/CameraFollow.cs
+++ Plataforma/Assets/Scripts/CameraFollow.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
+    public Transform player;      // arraste o jogador aqui no Inspector
     public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    public Vector3 offset;        // ajuste se quiser a câmera mais à frente ou acima
 
     void LateUpdate()
     {
-        Vector3 desiredPosition = new Vector3(
-            player.position.x + offset.x,
-            player.position.y + offset.y,
-            transform.position.z + offset.z
-        );
-
+        Vector3 desiredPosition = new Vector3(player.position.x + offset.x, transform.position.y, transform.position.z);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
     }
